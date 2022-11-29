95463c550056697c9265b142c9978612b6322188
Kenneth Imade <Kenneth.imade@yahoo.com>
2022-11-29 21:23:53 +0000

    refactor

--- 0x01-python-if_else_loops_functions/9-print_last_digit.py
+++ 0x01-python-if_else_loops_functions/9-print_last_digit.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
 
 def print_last_digit(number):
-    last_digit = str(number)[-1]
-    print("{}".format(last_digit), end="")
+    if number < 0:
+        number = -1 * number;
+    last_digit = number % 10
+    print(last_digit, end="")
     return (last_digit)
