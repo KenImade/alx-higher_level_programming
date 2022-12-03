b7a96cac4ec1af4615e674ff2a81f73d17fbfaa6
Kenneth Imade <Kenneth.imade@yahoo.com>
2022-12-03 15:58:50 +0000

    removed clause

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -1,8 +1,5 @@
 #!/usr/python3
 
 def print_matrix_integer(matrix=[[]]):
-    if len(matrix) < 0:
-        return None
-
     for row in matrix:
         print(' '.join(["{:d}".format(elem) for elem in row]))
