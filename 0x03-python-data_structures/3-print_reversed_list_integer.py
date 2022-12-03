8c7f292e30950547914c2da49030133909f67b6a
Kenneth Imade <Kenneth.imade@yahoo.com>
2022-12-03 15:22:31 +0000

    add escape clause

--- 0x03-python-data_structures/3-print_reversed_list_integer.py
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -1,6 +1,9 @@
 #!/usr/bin/python3
 
 def print_reversed_list_integer(my_list=[]):
+    if not my_list:
+        return
+
     my_list.reverse()
     for i in range(0, len(my_list)):
-        print("{}".format(my_list[i]))
+        print("{:d}".format(my_list[i]))
