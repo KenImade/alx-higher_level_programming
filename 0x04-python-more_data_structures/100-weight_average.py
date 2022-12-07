adddf1518ea990872cd6b1be1a1c7ae6ea53beb4
Kenneth Imade <Kenneth.imade@yahoo.com>
2022-12-07 22:26:55 +0000

    fixed zero clause

--- 0x04-python-more_data_structures/100-weight_average.py
+++ 0x04-python-more_data_structures/100-weight_average.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def weight_average(my_list=[]):
-    if my_list is None:
+    if len(my_list) == 0:
         return 0
     weighted_sum = sum(list(map(lambda x: x[0] * x[1], my_list)))
     weights = sum(list(map(lambda x: x[1], my_list)))
