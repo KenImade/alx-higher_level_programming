409d0781a50ae17cb76b8c3231d1a611a3657078
Kenneth Imade <Kenneth.imade@yahoo.com>
2022-12-07 22:04:05 +0000

    task 10 again

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -4,10 +4,4 @@ def best_score(a_dictionary):
     if a_dictionary is None or not a_dictionary:
         return "None"
     else:
-        max = 0
-        name = ""
-        for k, v in a_dictionary.items():
-            if a_dictionary[k] > max:
-                max = a_dictionary[k]
-                name = k
-        return name
+        return max(a_dictionary, key = lambda x: a_dictionary[x])
