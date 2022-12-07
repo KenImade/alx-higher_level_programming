19281aab4dc439e9c7ad8d47e13c8f055746ab0d
Kenneth Imade <Kenneth.imade@yahoo.com>
2022-12-07 21:29:24 +0000

    if else clause

--- 0x04-python-more_data_structures/8-simple_delete.py
+++ 0x04-python-more_data_structures/8-simple_delete.py
@@ -1,11 +1,9 @@
 #!/usr/bin/python3
 
 def simple_delete(a_dictionary, key=""):
-    if key == "":
+
+    if key in a_dictionary:
+        a_dictionary.pop(key)
         return a_dictionary
-
-    for k, v in a_dictionary.items():
-        if k == key:
-            del a_dictionary[key]
-
-    return a_dictionary
+    else:
+        return a_dictionary
